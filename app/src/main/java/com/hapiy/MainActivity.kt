659f4f6dacc1d5816740516102acaf85d68b2837
robovore <robovore@gmail.com>
2020-10-18 00:23:27 +1300

    Fix navigation buttons to only change fragment. Home button updates to home fragment.

--- app/src/main/java/com/hapiy/MainActivity.kt
+++ app/src/main/java/com/hapiy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hapiy
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -10,7 +12,12 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hapiy.ui.body.BodyFragment
+import com.hapiy.ui.create.CreateFragment
 import com.hapiy.ui.home.HomeFragment
+import com.hapiy.ui.home.HomeViewModel
+import com.hapiy.ui.mind.MindFragment
+import com.hapiy.ui.sleep.SleepFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,32 +26,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_sleep, R.id.navigation_body, R.id.navigation_home, R.id.navigation_mind, R.id.navigation_create))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(navListener)
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.nav_host_fragment,
+                HomeFragment()
+            ).commit()
+        }
 
 
 
     }
 
+    private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                var selectedFragment: Fragment? = null
+                when (item.getItemId()) {
+                    R.id.navigation_sleep -> selectedFragment = SleepFragment()
+                    R.id.navigation_body -> selectedFragment = BodyFragment()
+                    R.id.navigation_home -> selectedFragment = HomeFragment()
+                    R.id.navigation_mind -> selectedFragment = MindFragment()
+                    R.id.navigation_create -> selectedFragment = CreateFragment()
+                }
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.nav_host_fragment,
+                    selectedFragment!!
+                ).commit()
+                return true
+            }
+        }
+
     fun homeBtnClicked(v: View?) {
-//        val myIntent = Intent(this, HomeFragment::class.java)
-//        startActivity(intent)
-
         val fragment: Fragment = HomeFragment()
-
         val fm = supportFragmentManager
         val transaction: FragmentTransaction = fm.beginTransaction()
         transaction.replace(R.id.nav_host_fragment, fragment)
         transaction.commit()
 
     }
+
 
     fun getVisibleFragment(): Fragment? {
         val fragmentManager =
